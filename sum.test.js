a38ddea6c99567c1de269246d3aead422637f048
Munirov RR <munirov.rr@gmail.com>
2026-06-04 21:14:06 +0300

    add test

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum');
 test('adds 2 + 3', () => {
     expect(sum(2,3)).toBe(5);
 });
+
+test('adds 1 + 1', () => {
+    expect(sum(1,1)).toBe(2);
+});
+
+test('adds 10 + 5', () => {
+    expect(sum(10,5)).toBe(15);
+});
